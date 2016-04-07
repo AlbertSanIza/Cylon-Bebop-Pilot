46725858ee0bf41bb4949184f1b88ca2da8ad6b9
criszank <crisnchz94@gmail.com>
2016-04-07 10:23:45 -0700

    Xbox Controller Testing

--- xbox.js
+++ xbox.js
@@ -16,12 +16,12 @@ Cylon.robot({
         console.log("Button " + button + " released.");
       });
     });
-//    my.controller.on("left_x:move", function(pos) {
-//      console.log("Left Stick - X:", pos);
-//    });
-//    my.controller.on("left_y:move", function(pos) {
-//      console.log("Left Stick - Y:", pos);
-//    });
+    my.controller.on("left_x:move", function(pos) {
+      console.log("Left Stick - X:", pos);
+    });
+    my.controller.on("left_y:move", function(pos) {
+      console.log("Left Stick - Y:", pos);
+    });
     my.controller.on("right_x:move", function(pos) {
       console.log("Right Stick - X:", pos);
     });
